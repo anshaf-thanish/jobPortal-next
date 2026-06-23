d040d0a4ce1fcc1b628e25d68eee0b2f950d6b26
anshaf-thanish <anshafthanish@gmail.com>
2026-06-23 23:36:25 +0530

    nav

---
+++ constant/constant.ts
@@ -0,0 +1,8 @@
+export const NavLinks = [
+  { id: 1, label: "Home", url: "#" },
+  { id: 2, label: "Find Jobs", url: "#" },
+  { id: 3, label: "Candidates", url: "#" },
+  { id: 4, label: "Employers", url: "#" },
+  { id: 5, label: "Blog", url: "#" },
+  { id: 6, label: "Contact", url: "#" },
+];
